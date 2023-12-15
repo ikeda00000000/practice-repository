ecb302c11bdd87de41a444f92837d3a0b8f5ec95
ikeda.hikari <ikeda.hikari@alhinc.jp>
2023-12-15 18:10:57 +0900

    excel出力時にcaseの内容を反映するよう修正

--- CustomerManagementSystem/src/main/java/com/practice/CustomerManagementSystem/service/export/ExportToExcelService.java
+++ CustomerManagementSystem/src/main/java/com/practice/CustomerManagementSystem/service/export/ExportToExcelService.java
@@ -1,9 +1,12 @@
 package com.practice.CustomerManagementSystem.service.export;
 
 import java.io.OutputStream;
+import java.util.Date;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFCreationHelper;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
@@ -18,11 +21,61 @@ public class ExportToExcelService {
 		// sheetの作成
 		XSSFSheet sheet = wb.createSheet();
 		// 行・セルの生成
-		Row row = sheet.createRow(0);
-		Cell cell = row.createCell(0);
-
+		Row row1 = sheet.createRow(1);
+		Cell cell1_1 = row1.createCell(1);
+		Cell cell1_2 = row1.createCell(2);
+		cell1_1.setCellValue("印刷日時");
+		cell1_2.setCellValue(new Date());
+		// cell1_2の書式を日付型に設定
+		formatCellStyle(wb,cell1_2);
+		
+		Row row2 = sheet.createRow(2);
+		Cell cell2_1 = row2.createCell(1);
+		cell2_1.setCellValue("顧客情報");
+		
+		Row row3 = sheet.createRow(3);
+		Cell cell3_1 = row3.createCell(1);
+		Cell cell3_2 = row3.createCell(2);
+		cell3_1.setCellValue("顧客ID");
+		cell3_2.setCellValue(oneCase.getCustomerId());
+		
+		Row row4 = sheet.createRow(4);
+		Cell cell4_1 = row4.createCell(1);
+		Cell cell4_2 = row4.createCell(2);
+		cell4_1.setCellValue("顧客名");
+		cell4_2.setCellValue(oneCase.getCustomer().getCustomerName());
+		
+		Row row5 = sheet.createRow(5);
+		Cell cell5_1 = row5.createCell(1);
+		cell5_1.setCellValue("ケース情報");
+		
+		Row row6 = sheet.createRow(6);
+		Cell cell6_1 = row6.createCell(1);
+		Cell cell6_2 = row6.createCell(2);
+		cell6_1.setCellValue("ケース作成日");
+		cell6_2.setCellValue(oneCase.getCaseDate());
+		// cell6_2の書式を日付型に設定
+		formatCellStyle(wb,cell6_2);
+		
+		Row row7 = sheet.createRow(7);
+		Cell cell7_1 = row7.createCell(1);
+		Cell cell7_2 = row7.createCell(2);
+		cell7_1.setCellValue("ケース作成者");
+		cell7_2.setCellValue(oneCase.getAccount().getUsername());
+		
+		Row row8 = sheet.createRow(8);
+		Cell cell8_1 = row8.createCell(1);
+		Cell cell8_2 = row8.createCell(2);
+		cell8_1.setCellValue("タイトル");
+		cell8_2.setCellValue(oneCase.getTitle());
+		
+		Row row9 = sheet.createRow(9);
+		Cell cell9_1 = row9.createCell(1);
+		Cell cell9_2 = row9.createCell(2);
+		cell9_1.setCellValue("内容");
+		cell9_2.setCellValue(oneCase.getContent());
+		
 		try {
-			cell.setCellValue("テスト出力");
 			wb.write(outputStream);
 		} catch (Exception e) {
 			 e.printStackTrace();
@@ -33,5 +86,14 @@ public class ExportToExcelService {
 				 e.printStackTrace();
 			}
 		}
+	}
+	
+	// cellを日付型の書式に設定する
+	public void formatCellStyle(XSSFWorkbook wb, Cell cell) {
+		XSSFCreationHelper createHelper = wb.getCreationHelper();
+		XSSFCellStyle cellStyle = wb.createCellStyle();
+		short style = createHelper.createDataFormat().getFormat("yyyy/mm/dd h:mm");
+		cellStyle.setDataFormat(style);
+		cell.setCellStyle(cellStyle);
 	}
 }
